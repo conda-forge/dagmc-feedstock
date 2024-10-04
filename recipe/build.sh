1ffcfe366ebdad777a9744a85bb21f1475d7d6b9
bquan0 <powerbowen@gmail.com>
2024-10-04 13:25:22 -0500

    use curl instead of wget

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,8 +12,7 @@ fi
 if [[ "$dd" != "nodoubledown" ]]; then
   export CONFIGURE_ARGS="-DDOUBLE_DOWN=ON ${CONFIGURE_ARGS}"
   # install embree
-  sudo apt-get install wget
-  wget https://github.com/embree/embree/releases/download/v4.3.3/embree-4.3.3.x86_64.linux.tar.gz
+  curl -L https://github.com/embree/embree/releases/download/v4.3.3/embree-4.3.3.x86_64.linux.tar.gz > embree-4.3.3.x86_64.linux.tar.gz 
   tar xzf embree-4.3.3.x86_64.linux.tar.gz
   source embree-4.3.3.x86_64.linux/embree-vars.sh
   # clone double down repo
