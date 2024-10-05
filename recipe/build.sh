be27be68c861321dad36000f2ce74ca8f4922831
bquan0 <powerbowen@gmail.com>
2024-10-05 16:44:17 -0500

    specify location of double down for cmake

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,19 +10,20 @@ if [[ "$mpi" != "nompi" ]]; then
   export CONFIGURE_ARGS="-DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_C_COMPILER=mpicc ${CONFIGURE_ARGS}"
 fi
 if [[ "$dd" != "nodoubledown" ]]; then
-  export CONFIGURE_ARGS="-DDOUBLE_DOWN=ON ${CONFIGURE_ARGS}"
+  export CONFIGURE_ARGS="-DDOUBLE_DOWN=ON -Ddd_ROOT=${PREFIX}  ${CONFIGURE_ARGS}"
   # clone double down repo
-  git clone https://github.com/pshriwise/double-down
+  git clone -b v1.1.0 --depth 1 https://github.com/pshriwise/double-down.git
   cd double-down
   # configure the build
   mkdir bld
   cd bld
-  cmake .. -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_PREFIX_PATH="${PREFIX};${PREFIX}"
+  cmake .. -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DMOAB_DIR="${PREFIX}" -DEMBREE_DIR="${PREFIX}"
   # build and test double-down
   make all test
   # install
   make install
   cd ../..
+  rm -rf double-down
 else
   export CONFIGURE_ARGS="-DDOUBLE_DOWN=OFF ${CONFIGURE_ARGS}"
 fi
